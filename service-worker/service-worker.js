8e5b463180c67406a39e06059ece661cf034e2db
luoway <luoway@outlook.com>
2023-08-07 21:46:24 +0800

    service worker 生命周期

--- service-worker/service-worker.js
+++ service-worker/service-worker.js
@@ -1,33 +1,15 @@
 console.log('service worker loaded', self, globalThis)
 
 self.addEventListener('install', event => {
-    event.waitUntil(new Promise(()=>{
+    event.waitUntil(new Promise((resolve)=>{
         console.info('安装完成', self.clients)
+        setTimeout(resolve, 1000)
     }))
-    event.waitUntil(
-        self.clients.matchAll().then ( (client) => {
-            console.log(11, '行: client', client)
-            
-            client.postMessage({
-                msg: 'Hey, from service worker! I\'m listening to your fetch requests.',
-                source: 'service-worker'
-            })
-        })
-    )
 })
 
 self.addEventListener('activate', (event) => {
-    event.waitUntil(new Promise(()=>{
-        console.info('激活完成')
+    event.waitUntil(new Promise((resolve)=>{
+        console.info('激活完成', event)
+        setTimeout(resolve, 1000)
     }))
-    event.waitUntil(
-        self.clients.matchAll().then ( (client) => {
-            console.log(11, '行: client', client)
-            
-            client.postMessage({
-                msg: 'Hey, from service worker! I\'m listening to your fetch requests.',
-                source: 'service-worker'
-            })
-        })
-    )
 })
